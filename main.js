4c8e71ce372d6ed36203f76a6e1770a1ac71cf46
Aishu203100 <79292874+Aishu203100@users.noreply.github.com>
2022-04-19 19:04:04 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+Status = "";
+objects = [];
+
+function setup(){
+    canvas = createCanvas(380,380);
+    canvas.position(625,200);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function start(){
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status - Detecting Object";
+    input_text = document.getElementById("input_id").value;
+}
+function modelLoaded(){
+    console.log("Model Loaded!!!");
+    Status = true;
+}
+function draw(){
+    image(video,0,0,380,380);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
